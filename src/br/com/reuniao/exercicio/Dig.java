06dced95205ff44612e702afa205e7239a2efd7c
Rodrigo Barbosa Ferreira <you@example.com>
2019-06-26 23:25:44 -0300

    Exercio de laço de repetição e verificação IF

---
+++ src/br/com/reuniao/exercicio/Dig.java
@@ -0,0 +1,17 @@
+package br.com.reuniao.exercicio;
+
+public class Dig {
+	
+	public static void main(String[] args) {
+		
+	
+	
+	int gastosJaneiro = 15000;
+	int gastosFevereiro = 23000;
+	int gastosMarco = 17000;
+	int gastosTrimestre = gastosJaneiro + gastosFevereiro+gastosMarco;
+	
+	System.out.println(gastosTrimestre);
+				
+	}
+}
